e25a22ee923d939d356a471a02398498d71cffbc
saraEhab <sara3wahdan@gmail.com>
2019-03-22 21:28:58 +0200

    adding empty methods and variables to NFAToDFA.cpp and NFAToDFA.h

--- Compiler/NFAToDFA.cpp
+++ Compiler/NFAToDFA.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "NFAToDFA.h"
+
+void NFAToDFA::getNFARoot() {
+    nfaRoot = nonFiniteAutomata.getNFARoot();
+}
+
+void NFAToDFA::setDFANodeIndex(Node DFANode) {
+
+}
+
+vector<Node> NFAToDFA::findNodeTwins(Node nfaNode, Node dfaNode, Node parent) {}
+
+int NFAToDFA::isNodeExists(vector<Node> twins) {}
+
+bool NFAToDFA::isAcceptance(Node node) {}
+
+void NFAToDFA::setDifferentEdgeValues() {}
+
+Node NFAToDFA::getDFARoot() {}
+
+void NFAToDFA::makeChildren(Node DFANode) {}
+
+vector<Node> NFAToDFA::findChildrenForGivenInput(string inputValue, Node parent) {}
+
+void NFAToDFA::setNodeTwins(vector<Node> twins) {}
--- Compiler/NFAToDFA.h
+++ Compiler/NFAToDFA.h
@@ -6,11 +6,40 @@
 #define COMPILER_NFATODFA_H
 
 #include "Node.h"
+#include "NonFiniteAutomata.h"
 
 using namespace std;
 
 
 class NFAToDFA {
+private:
+    Node nfaRoot;
+    NonFiniteAutomata nonFiniteAutomata;
+    vector<vector<Node>> nodeTwins;
+    int lastTakenIndex = 0;
+    vector<string> differentEdgeValues;
+    vector<Node> DFA;
+
+public:
+    void getNFARoot();
+
+    void setDFANodeIndex(Node DFANode);
+
+    vector<Node> findNodeTwins(Node nfaNode, Node dfaNode, Node parent);
+
+    int isNodeExists(vector<Node> twins);
+
+    bool isAcceptance(Node node);
+
+    void setDifferentEdgeValues();
+
+    Node getDFARoot();
+
+    void makeChildren(Node DFANode);
+
+    vector<Node> findChildrenForGivenInput(string inputValue, Node parent);
+
+    void setNodeTwins(vector<Node> twins);
 };
 
 
