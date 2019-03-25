0453f1738f51d3835fffd451c9fc926b6fca951b
saraEhab <sara3wahdan@gmail.com>
2019-03-25 08:38:44 +0200

    transformation from nfa to dfa is done ^_^

--- Compiler/main.cpp
+++ Compiler/main.cpp
@@ -1,20 +1,6 @@
 #include <iostream>
 #include "NFAToDFAEngine.h"
 
-int n;
-//int add(int *n) {
-//    *n=10;
-//    return *n;
-//}
-
-void setn(int *k){
-    n=*k;
-}
-
-int getn(){
-    return n;
-}
-
 int main() {
     NFAToDFA nfaToDFA;
     nfaToDFA.engine();
@@ -22,25 +8,5 @@ int main() {
     dfaRoot=(nfaToDFA.dfa.getDFARoot());
     cout<<"we get it :D"<<endl;
 
-    ///////////////////////////////////
-int p=9;
-setn(&p);
-int h=getn();
-cout<<h<<endl;
-
-
-//    int i = 9;
-//     int *ptr;
-//    ptr = &i;
-//    // cout << add(ptr) << endl;/*the same as cout<<add(&i)<<endl;*/
-//    int j;
-//    j=add(ptr);
-//    cout<<j<<endl;
-//    add(&i);
-//     cout<<*ptr<<endl;
-//    cout << i << endl;
-//    //cout << &i << endl;
-//    //cout << *ptr << endl;
-//    //cout << *&i << endl;
     return 0;
 }
